ae734bc56725803dbb20c83eabaafca9fdb4564c
vitor-risso <vitor.risso13@gmail.com>
2021-04-06 19:27:59 -0300

    [000] initial commit, learning how to outpu and information on terminal without console.log

---
+++ index.js
@@ -0,0 +1,12 @@
+const questions = [
+  "O que aprendi hoje?",
+  "O que me deixou aborrecido?",
+  "O que me deixou feliz hoje?",
+  "Quantas pessoas ajudei ontem?"
+]
+
+const ask = (index = 0) => {
+  process.stdout.write(questions[index] + "\n\n\n")
+}
+
+ask()
